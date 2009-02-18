25a3638f14534ded9ebaac69da1fce3371a774fc
Jeremy Ellison <jeremy@3cubetechnologies.com>
2009-02-18 16:44:52 -0500

    whitespace

--- lib/active_security.rb
+++ lib/active_security.rb
@@ -3,3 +3,4 @@ require File.expand_path(File.dirname(__FILE__)) + '/active_security/role'
 require File.expand_path(File.dirname(__FILE__)) + '/active_security/role_set'
 require File.expand_path(File.dirname(__FILE__)) + '/active_security/model_permission'
 require File.expand_path(File.dirname(__FILE__)) + '/active_security/controller_permission'
+
